9a85216e14c2aa774e465e3fcf81b1ed1534dee5
Dennis Gilbert <dennis@pdx.edu>
2015-01-20 14:43:40 -0800

    Added token (re)sync feature

--- app/tokens.py
+++ app/tokens.py
@@ -47,23 +47,27 @@ class Tokens():
 			Update the cache with CRUD changes
 		'''
 		cache = StrictRedis(db=config.tokens_cache_redis_db)
+		pipe = cache.pipline()
 		
 		self.log.info('post_updates(): posting updates to local storage')
-		for update in updates:
+		for update in updates:			# Done: could use the Redis "Pipelines" feature to combine Redis requests for better performance
 			(user, token, date, action) = update
 			if action == 'add':
-				cache.set(token, user)	# user-by-token -- really just existence of a token
-				cache.set(user, token)	# token-by-user: allow lookup of previous token on token changes
+				pipe.cache.hset('general', token, user)	# user-by-token -- really just existence of a token
+				pipe.cache.hset('users', user, token)	# token-by-user: allow lookup of previous token on token changes
+				pipe.execute()
 				self.log.info('post_updates(): added token for user: ' + user)
 			elif action == 'delete':
-				cache.delete(token)	# disables the ability to authenticate
-				cache.delete(user)	# removes history of token
+				pipe.cache.hdel('general', token)	# disables the ability to authenticate
+				pipe.cache.hdel('users', user)	# removes history of token
+				pipe.execute()
 				self.log.info('post_updates(): deleted token for user: ' + user)
 			elif action == 'update':
-				prev_token = cache.get(user)
-				cache.delete(prev_token)	# disables the ability to authenticate with previous token
-				cache.set(token, user)		# set the new token for the user
-				cache.set(user, token)		# set the user as possessing the new token
+				prev_token = cache.hget('users', user)
+				pipe.cache.hdel('general', prev_token)	# disables the ability to authenticate with previous token
+				pipe.cache.hset('general', token, user)		# set the new token for the user
+				pipe.cache.hset('users', user, token)		# set the user as possessing the new token
+				pipe.execute()
 				self.log.info('post_updates(): updated token for user: ' + user)
 			else:
 				self.log.critical('post_updates(): unexpected change type: ' + action)
@@ -111,10 +115,65 @@ class Tokens():
 			self.log.error('fetch_delta(): exception: ' + str(ex))
 			
 		return updates
-			
+	
+	def sync_cache(self):
+		'''
+			Erase the local cache of tokens and scopes, and reload from cloud storage
+		'''
 		
+		# Collect Token management info from the cloud
+		client = gdata.spreadsheet.service.SpreadsheetsService()
+		client.ClientLogin(config.tokens_google_client_login, config.tokens_google_client_password)
+		worksheets_feed = client.GetWorksheetsFeed(config.tokens_spreadsheet_id).entry
+		worksheet_ids = {}
+		for worksheet in worksheets_feed:
+			worksheet_ids[worksheet.title.text] = worksheet.id.text.split('/')[-1]
+			self.log.debug('sync_cache() found sheets: ' + worksheet.title.text)
 		
+		# Delete 'general' cache state
+		cache = StrictRedis(db=config.tokens_cache_redis_db)
+		cache.flushdb()	# Remove all kes from the current database
+		
+		# Fetch token list
+		query = gdata.spreadsheet.service.CellQuery()
+		query.min_row = '1'
+		cells = client.GetCellsFeed(config.tokens_spreadsheet_id, wksht_id=worksheet_ids['tokens'], query=query).entry
+		cols = 3
+		rows = len(cells) / cols
+		tokens = []
+		for row in range(0, rows):
+			tokens.append([str(cell.content.text) for cell in cells[row * cols : (row + 1) * cols]])
+		self.log.debug('sync_cache() fetched tokens from cloud: ' + str(rows))
+		
+		# Fetch scopes
+		scopes = {}
+		
+		for worksheet_title in worksheet_ids.keys():
+			if worksheet_title not in ['tokens', 'change log']:	# must then be a scope list
+				self.log.debug('sync_cache() fetching scope: ' + str(worksheet_title))
+				scopes[worksheet_title] = []
+				cells = client.GetCellsFeed(config.tokens_spreadsheet_id, wksht_id=worksheet_ids[worksheet_title], query=query).entry
+				for cell in cells:
+					scopes[worksheet_title].append(cell.content.text)
+				self.log.debug('sync_cache() fetched scope: ' + str(worksheet_title) + ', with number of items: ' + str(len(cells)))
+					
+		# Build user and general cache
+		for token_ent in tokens:
+			(user, token, datetime) = token_ent
+			cache.set(token, user)
+		self.log.debug('sync_cache() added tokens, count: ' + str(len(tokens)))
+		
+		# Build scopes
+		for (scope_name, scope_list) in scopes.items():
+			for user in scope_list:
+				cache.sadd(scope_name, user)
+			self.log.debug('sync_cache() added scope: ' + scope_name + ', with member count: ' + str(len(scope_list)))
 
+		# Reset the cache log index to the last entry
+		num_log_entries = len(client.GetListFeed( config.tokens_spreadsheet_id, wksht_id=worksheet_ids['change log']).entry) + 1
+		cache.set('log_index', num_log_entries) # Reset the cache log index to the start
+		self.log.debug('sync_cache() set log_index to: ' + str(num_log_entries))
+		
 		
 '''
 if __name__ == '__main__':	
--- auth.py
+++ auth.py
@@ -20,10 +20,16 @@ def check_auth(login, password, required_scope):
 	login_hash = base64.encodestring( hash_raw.digest()).strip()
 	
 	cache = StrictRedis(db=config.tokens_cache_redis_db)	# TODO: Should be at application scope instead of request scope
-	user = cache.get(required_scope + login_hash)	# lookup our person
-	
-	return not (user is None)
-
+	user = cache.get(login_hash)	# lookup our person
+	if (user is None):				# if the user has a valid token
+		return False
+	elif (required_scope == 'general'):
+		return True
+	elif (cache.sismember(required_scope, user) is None):
+		return False				# but the user does not have the required scope
+	else:
+		return True					# and the user has the required scope
+		
 def authenticate():
 	"""Sends a 401 response that enables basic auth"""
 	return Response(

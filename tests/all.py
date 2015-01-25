a277c6993b0b9c3bc20216f3e16b0a45c54b5e77
dennis <dennis@pdx.edu>
2015-01-25 08:31:25 -0800

    finished adding all tests to the test suite

--- tests/all.py
+++ tests/all.py
@@ -7,22 +7,23 @@ import unittest
 import os
 import re
 
-class TestAll(unittest.TestCase):
+#class TestAll(unittest.TestCase):
     
 
-    def suite(self):
-        files = os.listdir('.')
-        test_files_re = re.compile("test\.py$", re.IGNORECASE)
-        files = filter(test_files_re.search, files)
-        filenameToModuleName = lambda f: os.path.splitext(f)[0]
-        moduleNames = map(filenameToModuleName, files)
-        modules = map(__import__, moduleNames)
-        load = unittest.defaultTestLoader.loadTestsFromModule
-        return unittest.TestSuite(map(load, modules))
+def finti_suite():
+    files = os.listdir('.')
+    test_files_re = re.compile("test.*\.py$", re.IGNORECASE)
+    files = filter(test_files_re.search, files)
+    filenameToModuleName = lambda f: os.path.splitext(f)[0]
+    moduleNames = map(filenameToModuleName, files)
+    modules = map(__import__, moduleNames)
+    load = unittest.defaultTestLoader.loadTestsFromModule
+    
+    return unittest.TestSuite(map(load, modules))
     
         #building_auth = unittest.TestLoader().loadTestsFromModule('buildings_auth')
 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main(defaultTest='suite')
+    unittest.main(defaultTest='finti_suite')
